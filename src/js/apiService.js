b6427ea80e34460f81fde213b796795dbe4c9204
Oleksandr-Smetana <anatemso@gmail.com>
2021-08-25 15:17:02 +0300

    добавляет рендер карточек

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -6,26 +6,27 @@ export default class PhotosApiService {
 
   fetchPhotos() {
     const staticUrl = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
+    // console.log(this);
 
-    console.log(this);
-
-    fetch(
+    return fetch(
       `${staticUrl}&q=${this.searchQuery}&page=${this.page}&per_page=12&key=23038692-8ee91ca42b74ab69b2665b678`,
     )
       .then(response => response.json())
-      .then(data => {
-        console.log(this);
+      .then(({ hits }) => {
+        // console.log(data.hits);
         this.page += 1;
+
+        return hits;
       });
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 
   //   get query() {
   //     return this.searchQuery;
   //   }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
 
   //   resetPage() {
   //     this.page = 1;
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,16 @@
+import photoCardTpl from '../templates/photo-card.hbs';
 import PhotosApiService from './apiService';
 
 const refs = {
   searchFormElt: document.querySelector('#search-form'),
-  searchMoreButtonElt: document.querySelector('#search-more-button'),
+  searchMoreButtonElt: document.querySelector('#load-more-button'),
   galleryElt: document.querySelector('.gallery'),
 };
 
 const photosApiService = new PhotosApiService();
 
 refs.searchFormElt.addEventListener('submit', onSearch);
-refs.searchMoreButtonElt.addEventListener('click', onSearchMore);
+refs.searchMoreButtonElt.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
@@ -17,9 +18,13 @@ function onSearch(e) {
   photosApiService.query = e.currentTarget.elements.query.value;
   photosApiService.page = 1;
   //   photosApiService.resetPage();
-  photosApiService.fetchPhotos();
+  photosApiService.fetchPhotos().then(createPhotoCardMarkup);
 }
 
-function onSearchMore(e) {
-  photosApiService.fetchPhotos();
+function onLoadMore(e) {
+  photosApiService.fetchPhotos().then(createPhotoCardMarkup);
+}
+
+function createPhotoCardMarkup(hits) {
+  refs.galleryElt.insertAdjacentHTML('beforeend', photoCardTpl(hits));
 }
